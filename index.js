40a1144d774636d2469ebe6960e507416744fb88
Santhoshmahanthi123 <singumahanthisanthosh@gmail.com>
2019-03-09 23:28:20 +0530

    send api created

--- index.js
+++ index.js
@@ -25,6 +25,13 @@ app.post("/webhook", (req, res) => {
       // Get the sender PSID
       let sender_psid = webhook_event.sender.id;
       console.log("Sender PSID: " + sender_psid);
+      // Check if the event is a message or postback and
+      // pass the event to the appropriate handler function
+      if (webhook_event.message) {
+        handleMessage(sender_psid, webhook_event.message);
+      } else if (webhook_event.postback) {
+        handlePostback(sender_psid, webhook_event.postback);
+      }
     });
 
     // Return a '200 OK' response to all events
@@ -58,13 +65,35 @@ app.get("/webhook", (req, res) => {
   }
 });
 // Handles messages events
-function handleMessage(sender_psid, received_message) {}
+function handleMessage(sender_psid, received_message) {
+  let response;
+
+  // Check if the message contains text
+  if (received_message.text) {
+    // Create the payload for a basic text message
+    response = {
+      text: `You sent the message: "${
+        received_message.text
+      }". Now send me an image!`
+    };
+  }
+
+  // Sends the response message
+  callSendAPI(sender_psid, response);
+}
 
 // Handles messaging_postbacks events
 function handlePostback(sender_psid, received_postback) {}
 
 // Sends response messages via the Send API
-function callSendAPI(sender_psid, response) {}
+function callSendAPI(sender_psid, response) {
+  // Construct the message body
+  let request_body = {
+    "recipient": {
+      "id": sender_psid
+    },
+    "message": response
+}
 
 // Sets server port and logs message on success
 app.listen(process.env.PORT || 1337, () => console.log("webhook is listening"));
