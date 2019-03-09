6807e63553ec9bf4df661de9099fbfceab034bc6
Santhoshmahanthi123 <singumahanthisanthosh@gmail.com>
2019-03-10 00:13:49 +0530

    sending generic template

--- index.js
+++ index.js
@@ -74,7 +74,7 @@ function handleMessage(sender_psid, received_message) {
 
   // Checks if the message contains text
   if (received_message.text) {
-    // Creates the payload for a basic text message, which
+    // Create the payload for a basic text message, which
     // will be added to the body of our request to the Send API
     response = {
       text: `You sent the message: "${
@@ -82,11 +82,38 @@ function handleMessage(sender_psid, received_message) {
       }". Now send me an attachment!`
     };
   } else if (received_message.attachments) {
-    // Gets the URL of the message attachment
+    // Get the URL of the message attachment
     let attachment_url = received_message.attachments[0].payload.url;
+    response = {
+      attachment: {
+        type: "template",
+        payload: {
+          template_type: "generic",
+          elements: [
+            {
+              title: "Is this the right picture?",
+              subtitle: "Tap a button to answer.",
+              image_url: attachment_url,
+              buttons: [
+                {
+                  type: "postback",
+                  title: "Yes!",
+                  payload: "yes"
+                },
+                {
+                  type: "postback",
+                  title: "No!",
+                  payload: "no"
+                }
+              ]
+            }
+          ]
+        }
+      }
+    };
   }
 
-  // Sends the response message
+  // Send the response message
   callSendAPI(sender_psid, response);
 }
 
