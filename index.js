f1efecbfd4eb97dddeae6a2356350159a00c1674
Santhoshmahanthi123 <singumahanthisanthosh@gmail.com>
2019-03-09 21:19:13 +0530

    verification token changed

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.post("/webhook", (req, res) => {
 // Adds support for GET requests to our webhook
 app.get("/webhook", (req, res) => {
   // Your verify token. Should be a random string.
-  let VERIFY_TOKEN = "<YOUR_VERIFY_TOKEN>";
+  let VERIFY_TOKEN = "UMRK";
 
   // Parse the query params
   let mode = req.query["hub.mode"];
