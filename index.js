ba5e248123d3019100a4e177f42d747ecd930c5e
Santhoshmahanthi123 <singumahanthisanthosh@gmail.com>
2019-03-13 14:10:37 +0530

    payload feed changed

--- index.js
+++ index.js
@@ -140,11 +140,105 @@ function handlePostback(sender_psid, received_postback) {
 
   // Set the response based on the postback payload
   if (payload === "A") {
-    response = { text: "You have opted for Walkins!!" };
+    response = {
+      attachment: {
+        type: "template",
+        payload: {
+          template_type: "generic",
+          elements: [
+            {
+              title: "Welcome to Flying Sphaghetti Monster Restaurant!",
+              subtitle: "Select the time of your convinience for walkin!.",
+              image_url: attachment_url,
+              buttons: [
+                {
+                  type: "postback",
+                  title: "4 PM",
+                  payload: "D"
+                },
+                {
+                  type: "postback",
+                  title: "5 PM",
+                  payload: "E"
+                },
+                {
+                  type: "postback",
+                  title: "6 PM",
+                  payload: "F"
+                }
+              ]
+            }
+          ]
+        }
+      }
+    };
   } else if (payload === "B") {
-    response = { text: "You have opted for Reservation!." };
+    response = {
+      attachment: {
+        type: "template",
+        payload: {
+          template_type: "generic",
+          elements: [
+            {
+              title: "Welcome to Flying Sphaghetti Monster Restaurant!",
+              subtitle:
+                "Select the time of your convinience for reserving your table!.",
+              image_url: attachment_url,
+              buttons: [
+                {
+                  type: "postback",
+                  title: "7 PM",
+                  payload: "G"
+                },
+                {
+                  type: "postback",
+                  title: "8 PM",
+                  payload: "H"
+                },
+                {
+                  type: "postback",
+                  title: "9 PM",
+                  payload: "I"
+                }
+              ]
+            }
+          ]
+        }
+      }
+    };
   } else if (payload === "C") {
-    response = { text: "You have opted for Feed back!" };
+    response = {
+      attachment: {
+        type: "template",
+        payload: {
+          template_type: "generic",
+          elements: [
+            {
+              title: "Welcome to Flying Sphaghetti Monster Restaurant!",
+              subtitle: "Give Us Rating by opting below options!",
+              image_url: attachment_url,
+              buttons: [
+                {
+                  type: "postback",
+                  title: "Best!",
+                  payload: "J"
+                },
+                {
+                  type: "postback",
+                  title: "Better!",
+                  payload: "K"
+                },
+                {
+                  type: "postback",
+                  title: "Good!",
+                  payload: "L"
+                }
+              ]
+            }
+          ]
+        }
+      }
+    };
   }
   // Send the message to acknowledge the postback
   callSendAPI(sender_psid, response);
