238791764bfc733640dac7755b1b97e317f0f341
techgeek123 <singumahanthisanthosh@gmail.com>
2019-03-21 16:53:08 +0530

    types changes

--- controllers/answers.js
+++ controllers/answers.js
@@ -19,7 +19,7 @@ exports.post_answer = (req, res) => {
           options.map(option => {
             if (option.id == result.optionId) {
               // console.log("matched!");
-              Types.findById()
+              Types.find()
                 .exec()
                 .then(display => {
                   console.log(
