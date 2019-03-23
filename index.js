fb2f3a83fe7c8d65c2653ab926d175ae0216fe2a
techgeek123 <singumahanthisanthosh@gmail.com>
2019-03-23 16:43:01 +0530

    logs added

--- index.js
+++ index.js
@@ -217,7 +217,7 @@ function callSendAPI(sender_psid, response) {
     },
     message: response
   };
-  console.log(request_body);
+  console.log(JSON.stringify(request_body));
 
   // Send the HTTP request to the Messenger Platform
   request(
