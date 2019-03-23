1d9438e76903894b47863644e3c63e99cfdadc97
techgeek123 <singumahanthisanthosh@gmail.com>
2019-03-23 15:52:55 +0530

    changed res

--- index.js
+++ index.js
@@ -165,6 +165,7 @@ function handlePostback(sender_psid, received_postback) {
   if (payload_key === "A") {
     // response = { text: "You have opted for Walkins!!" };
     webhook.create_webhook(payload_value, res => {
+      console.log(res);
       let options = [];
       res.map(item => {
         options.push({
